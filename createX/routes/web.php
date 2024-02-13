1f7bacde30475d24fd02b02417383d93a652bbe6
gabilgg07 <gabilgg07@gmail.com>
2024-02-13 13:32:24 +0400

    Added home template ans assets

--- createX/routes/web.php
+++ createX/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'control', 'as' => 'admin.'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 });
